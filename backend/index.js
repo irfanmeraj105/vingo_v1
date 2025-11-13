b35cfdf931caf576efa12c9995ad5cb46e64a906
irfan meraj <postswapnetwork@gmail.com>
2025-11-13 07:56:57 +0500

    cors setup

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 dotenv.config();
 const authRoutes = require("./routes/auth.routes");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -11,7 +12,13 @@ const PORT = process.env.PORT || 5001;
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 // db connection
 connectDB();
---
+++ frontend/src/app/signup/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    
+    <div>
+
+        
+
+    </div>
+
+    </>
+  )
+}
+
+export default page
